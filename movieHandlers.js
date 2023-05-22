c75e06529520ca4de2edca6f12a8eb4f9b97fe0d
Adèle Manga <adelemanga75@gmail.com>
2023-05-22 10:10:40 +0200

    i am submit the commits

--- movieHandlers.js
+++ movieHandlers.js
@@ -25,21 +25,6 @@ const users = [
   },
 ];
 
-// const getMovies = (req, res) => {
-//   res.json(movies);
-// };
-
-// const getMovieById = (req, res) => {
-//   const id = parseInt(req.params.id);
-
-//   const movie = movies.find((movie) => movie.id === id);
-
-//   if (movie != null) {
-//     res.json(movie);
-//   } else {
-//     res.status(404).send("Not Found");
-//   }
-// };
 
 const getUsers = (req, res) => {
   res.status(202).json(movies.users);
