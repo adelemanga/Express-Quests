666778fc35396b890d5671aafa427e35409ecd46
Adèle Manga <adelemanga75@gmail.com>
2023-05-22 09:56:08 +0200

    i an adding the express 2, exercice

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const port = 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", movieHandlers.getUsers);
+app.get("api/users/:id", movieHandlers.getUsersById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
